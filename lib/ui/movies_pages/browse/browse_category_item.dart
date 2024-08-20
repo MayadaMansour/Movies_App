7420c142b0883f7d8bf3179dc521c66efd069194
Salma-Yousry <salmayousry779@gmail.com>
2024-08-20 06:39:36 +0300

    Ui BrowseScreen

---
+++ lib/ui/movies_pages/browse/browse_category_item.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:moves_app_project/ui/utils/color_resource/color_resources.dart';
+
+class BrowseCategoryItem extends StatelessWidget {
+  final String title;
+
+  BrowseCategoryItem({required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 10 / 7,
+      child: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: ColorResources.grey,
+              borderRadius: BorderRadius.circular(9.0),
+            ),
+            child: Center(
+              child: Icon(
+                Icons.image,
+                color: ColorResources.white,
+                size: 50,
+              ),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: ColorResources.bgColor.withOpacity(0.5),
+              borderRadius: BorderRadius.circular(9.0),
+            ),
+            alignment: Alignment.center,
+            child: Text(
+              title,
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

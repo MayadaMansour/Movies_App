c62ac7a2aa7a25e63d00e2bd6832a19e752cd06b
Hanya <hanyaamr2003@gmail.com>
2024-08-24 14:43:17 +0300

    Search Screen Ui and Logic.

---
+++ lib/ui/movies_pages/search/api/SearchResponse.dart
@@ -0,0 +1,130 @@
+/// page : 1
+/// results : [{"adult":false,"backdrop_path":"/5deKXeVmu3G8821gV8DtKHmRd6r.jpg","genre_ids":[53,18],"id":958196,"original_language":"en","original_title":"Inside","overview":"An art thief becomes trapped in a New York penthouse after his heist goes awry. Imprisoned with nothing but priceless works of art, he must use all his cunning and invention to survive.","popularity":23.603,"poster_path":"/dXsiWJWwGwYwOQ6DfYFt5pPBMwT.jpg","release_date":"2023-03-09","title":"Inside","video":false,"vote_average":5.64,"vote_count":332},{"adult":false,"backdrop_path":"/p5ozvmdgsmbWe0H8Xk7Rc8SCwAB.jpg","genre_ids":[16,10751,12,35],"id":1022789,"original_language":"en","original_title":"Inside Out 2","overview":"Teenager Riley's mind headquarters is undergoing a sudden demolition to make room for something entirely unexpected: new Emotions! Joy, Sadness, Anger, Fear and Disgust, who’ve long been running a successful operation by all accounts, aren’t sure how to feel when Anxiety shows up. And it looks like she’s not alone.","popularity":3120.278,"poster_path":"/dbhN0rEVTefOOgtuqEQwp4tXr0X.jpg","release_date":"2024-06-11","title":"Inside Out 2","video":false,"vote_average":7.7,"vote_count":2761},{"adult":false,"backdrop_path":"/auYFCeQ5iCywocANmIpAg5f4Zyz.jpg","genre_ids":[27,53],"id":365995,"original_language":"en","original_title":"Inside","overview":"Completely on her own, Sarah spends her time at home, waiting for the approaching birth of her child. But one night, a stranger breaks into her home, ready to snatch her still unborn baby from her.","popularity":10.942,"poster_path":"/1PSctD5yezkDKl5j61kKOk05U0g.jpg","release_date":"2016-02-14","title":"Inside","video":false,"vote_average":5.033,"vote_count":215},{"adult":false,"backdrop_path":null,"genre_ids":[18,27,9648],"id":136146,"original_language":"en","original_title":"Inside","overview":"A mentally ill patient is questioned to see if he is fit for release.","popularity":6.457,"poster_path":"/gUwRJkH1CCS88W3k8TC7yQYT5YU.jpg","release_date":"2002-06-01","title":"Inside","video":false,"vote_average":7,"vote_count":36},{"adult":false,"backdrop_path":"/fCUMcvj9FzkiWJi3uyYiUPTlmKo.jpg","genre_ids":[80,53],"id":1225582,"original_language":"en","original_title":"Inside","overview":"Mel is transferred from a juvenile to an adult jail and is taken under the wing of Mark, Australia's most hated criminal, and Warren, a soon-to-be-paroled inmate. The parental love triangle that forms between them becomes their doom.","popularity":4.372,"poster_path":"/tvmRLyZUz3DuPdtZk8Fbp1YhJ3O.jpg","release_date":"2025-02-27","title":"Inside","video":false,"vote_average":0,"vote_count":0},{"adult":false,"backdrop_path":"/iQdklg4pxPLatJWq9xcr5vlcALM.jpg","genre_ids":[27,18],"id":1305312,"original_language":"en","original_title":"Inside","overview":"\"INSIDE\" is a short film that explores the internal struggle of a young man battling depression and self-doubt. The Protagonist, exhausted and overwhelmed, retreats to his chaotic room, where echoes of past criticisms torment him. He drifts into a surreal nightmare world where he confronts a shadowy figure, the Antagonist, representing his inner demons. Urged to give up and end his life, the Protagonist ultimately finds the strength to resist, symbolically defeating his negative thoughts. Awakening with renewed determination, he begins to clean his room, reflecting his journey towards mental clarity and positivity. The film ends on a hopeful note, showcasing the Protagonist's resilience and transformation.","popularity":0.431,"poster_path":"/y14tdopBSgTot5SCNLHRN58v3bY.jpg","release_date":"2024-06-13","title":"Inside","video":false,"vote_average":7.5,"vote_count":2},{"adult":false,"backdrop_path":null,"genre_ids":[18,27,53],"id":15398,"original_language":"en","original_title":"Inside","overview":"Alex has a disturbing hobby of following strangers...until one night he gets caught. Instead of calling the police, the couple--fascinated by Alex's resemblance to their dead son--asks him to stay, and Alex finds himself trapped in a horrifying existence of desperation, despair, and insanity.","popularity":7.035,"poster_path":"/8aB4QdSeDP0UKdJaQmYgR0ZrFax.jpg","release_date":"2006-03-26","title":"Inside","video":false,"vote_average":4.5,"vote_count":17},{"adult":false,"backdrop_path":"/j29ekbcLpBvxnGk6LjdTc2EI5SA.jpg","genre_ids":[16,10751,12,18,35],"id":150540,"original_language":"en","original_title":"Inside Out","overview":"When 11-year-old Riley moves to a new city, her Emotions team up to help her through the transition. Joy, Fear, Anger, Disgust and Sadness work together, but when Joy and Sadness get lost, they must journey through unfamiliar places to get back home.","popularity":388.243,"poster_path":"/2H1TmgdfNtsKlU9jKdeNyYL5y8T.jpg","release_date":"2015-06-17","title":"Inside Out","video":false,"vote_average":7.914,"vote_count":21484},{"adult":false,"backdrop_path":null,"genre_ids":[27],"id":168901,"original_language":"en","original_title":"Inside","overview":"Prison inmate Miles Barrett is ripped awake in the middle of the night by a terrifying crash, gunfire, and the sickening screams of a man being torn apart. Something is loose in the prison - and it's coming for Miles...","popularity":5.153,"poster_path":"/sVJrVPnt1qSf7UueybSlJK3WQ8z.jpg","release_date":"2012-01-11","title":"Inside","video":false,"vote_average":4,"vote_count":16},{"adult":false,"backdrop_path":null,"genre_ids":[53],"id":865616,"original_language":"en","original_title":"Inside","overview":"Christina Perasso is a tough, resilient, 24-year old girl. She wakes up trapped in a room and has no idea where she is being held or who did this to her. She has been left access to her laptop. Through the use of an intermittent Wi-Fi signal, she reaches out to friends, family and the general, growing audience through social media. She lists facts, clues, pictures and videos to aid the audience in figuring out where she is, who her captor is, and why she was kidnapped.","popularity":0.645,"poster_path":"/cRmz5pDytWkZJyd212fa7K4reYd.jpg","release_date":"2011-09-06","title":"Inside","video":false,"vote_average":0,"vote_count":0},{"adult":false,"backdrop_path":"/2HqbXDU5v8U7b7rtNI8A6kcUdF5.jpg","genre_ids":[27,18],"id":811074,"original_language":"en","original_title":"Inside","overview":"Trapped in an apartment with a menacing creature, a man has to undergo a cyclical routine to please it and keep himself alive. However, his captivity eventually becomes unsustainable.","popularity":0.049,"poster_path":"/c1gagLlo9ZI5F3ieRqvcKOlGEvT.jpg","release_date":"","title":"Inside","video":false,"vote_average":10,"vote_count":1},{"adult":false,"backdrop_path":null,"genre_ids":[27],"id":862212,"original_language":"en","original_title":"Inside","overview":"Strange things happen when an ordinary guy that lives alone slowly starts to realise that maybe he isn't...","popularity":0.001,"poster_path":"/gflqmxCqY9kyzcHcFqNbOBRw541.jpg","release_date":"","title":"Inside","video":false,"vote_average":0,"vote_count":0},{"adult":false,"backdrop_path":null,"genre_ids":[80,18,10770],"id":303693,"original_language":"en","original_title":"Inside","overview":"A South African political prisoner is tortured to obtain information on apartheid conspirators. Ten years later, the head officer in charge of the questioning is similarly held as prisoner and questioned about his past offenses.","popularity":7.826,"poster_path":"/qD3NPLfWXp1c3YBDc9mmht8B81C.jpg","release_date":"1996-08-25","title":"Inside","video":false,"vote_average":4.6,"vote_count":7},{"adult":false,"backdrop_path":null,"genre_ids":[],"id":834863,"original_language":"en","original_title":"Inside","overview":"Yann Chapotel meticulously rearranged live-action shots from a multi-story residential building into a formally rigid montage movie—featuring a slew of surprising twists and turns along the way. At the same time, we observe people oscillating between interiors and exteriors, performing everyday activities on balconies and by the open window, which Chapotel combines into a collective choreography.","popularity":0.097,"poster_path":"/fhHpAUduYW4HsQcnNd6PqWI9eQx.jpg","release_date":"2021-08-25","title":"Inside","video":false,"vote_average":4,"vote_count":2},{"adult":false,"backdrop_path":null,"genre_ids":[],"id":446608,"original_language":"de","original_title":"Inside","overview":"","popularity":0.669,"poster_path":"/7NF87Mla45VTIhBo2Y1cbJjHJQY.jpg","release_date":"2005-05-07","title":"Inside","video":false,"vote_average":7,"vote_count":2},{"adult":false,"backdrop_path":null,"genre_ids":[],"id":491595,"original_language":"en","original_title":"Inside","overview":"Experimental feature.","popularity":0.303,"poster_path":null,"release_date":"2017-02-02","title":"Inside","video":false,"vote_average":0,"vote_count":0},{"adult":false,"backdrop_path":null,"genre_ids":[9648],"id":1061946,"original_language":"en","original_title":"Inside","overview":"Inside, is a mystery short film shot in an abandoned house and inspired by possible true events (prequel of another short film wich will be realise soon).\r Watch it until the end of the credits and wear headphones for a better sound.","popularity":0.001,"poster_path":null,"release_date":"","title":"Inside","video":false,"vote_average":0,"vote_count":0},{"adult":false,"backdrop_path":null,"genre_ids":[878,16,18],"id":502942,"original_language":"en","original_title":"Inside","overview":"Dive into a monochrome ‘sketch’ animation detailing the end of humanity as we know it.","popularity":0.33,"poster_path":"/6lBA3MO86IGBWacuWL6vok5OO8S.jpg","release_date":"2016-05-23","title":"Inside","video":false,"vote_average":9,"vote_count":1},{"adult":false,"backdrop_path":null,"genre_ids":[],"id":1122340,"original_language":"en","original_title":"Inside","overview":"A young man experiences strange events in his room after a voice inviting him to enter the room.","popularity":0.052,"poster_path":"/aJAv5lA9ONitp9qVlJeq6nq9hC7.jpg","release_date":"2022-12-12","title":"Inside","video":false,"vote_average":0,"vote_count":0},{"adult":false,"backdrop_path":null,"genre_ids":[53],"id":280054,"original_language":"en","original_title":"Inside","overview":"Two doctors are observing a very \"special patient\" through the small window of her cell in the deep, dark corners of a mental asylum. Each morning, at exactly the same time, she performs the exact same extraordinary routine.","popularity":0.685,"poster_path":null,"release_date":"2013-02-17","title":"Inside","video":false,"vote_average":3.7,"vote_count":3}]
+/// total_pages : 83
+/// total_results : 1646
+
+class SearchResponse {
+  SearchResponse({
+      this.page,
+      this.results,
+      this.totalPages,
+      this.totalResults,
+   this.statusCode,
+  this.statusMessage,
+  this.success});
+
+  SearchResponse.fromJson(dynamic json) {
+    page = json['page'];
+    if (json['results'] != null) {
+      results = [];
+      json['results'].forEach((v) {
+        results?.add(SearchResults.fromJson(v));
+      });
+    }
+    totalPages = json['total_pages'];
+    totalResults = json['total_results'];
+  }
+  int? page;
+  List<SearchResults>? results;
+  String? success;
+  String? statusMessage;
+  int? statusCode;
+  int? totalPages;
+  int? totalResults;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['page'] = page;
+    if (results != null) {
+      map['results'] = results?.map((v) => v.toJson()).toList();
+    }
+    map['total_pages'] = totalPages;
+    map['total_results'] = totalResults;
+    return map;
+  }
+
+}
+
+/// adult : false
+/// backdrop_path : "/5deKXeVmu3G8821gV8DtKHmRd6r.jpg"
+/// genre_ids : [53,18]
+/// id : 958196
+/// original_language : "en"
+/// original_title : "Inside"
+/// overview : "An art thief becomes trapped in a New York penthouse after his heist goes awry. Imprisoned with nothing but priceless works of art, he must use all his cunning and invention to survive."
+/// popularity : 23.603
+/// poster_path : "/dXsiWJWwGwYwOQ6DfYFt5pPBMwT.jpg"
+/// release_date : "2023-03-09"
+/// title : "Inside"
+/// video : false
+/// vote_average : 5.64
+/// vote_count : 332
+
+class SearchResults {
+  SearchResults({
+      this.adult,
+      this.backdropPath,
+      this.genreIds,
+      this.id,
+      this.originalLanguage,
+      this.originalTitle,
+      this.overview,
+      this.popularity,
+      this.posterPath,
+      this.releaseDate,
+      this.title,
+      this.video,
+      this.voteAverage,
+      this.voteCount,});
+
+  SearchResults.fromJson(dynamic json) {
+    adult = json['adult'];
+    backdropPath = json['backdrop_path'];
+    genreIds = json['genre_ids'] != null ? json['genre_ids'].cast<int>() : [];
+    id = json['id'];
+    originalLanguage = json['original_language'];
+    originalTitle = json['original_title'];
+    overview = json['overview'];
+    popularity = json['popularity'];
+    posterPath = json['poster_path'];
+    releaseDate = json['release_date'];
+    title = json['title'];
+    video = json['video'];
+    voteAverage = json['vote_average'];
+    voteCount = json['vote_count'];
+  }
+  bool? adult;
+  String? backdropPath;
+  List<int>? genreIds;
+  int? id;
+  String? originalLanguage;
+  String? originalTitle;
+  String? overview;
+  double? popularity;
+  String? posterPath;
+  String? releaseDate;
+  String? title;
+  bool? video;
+  double? voteAverage;
+  int? voteCount;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['adult'] = adult;
+    map['backdrop_path'] = backdropPath;
+    map['genre_ids'] = genreIds;
+    map['id'] = id;
+    map['original_language'] = originalLanguage;
+    map['original_title'] = originalTitle;
+    map['overview'] = overview;
+    map['popularity'] = popularity;
+    map['poster_path'] = posterPath;
+    map['release_date'] = releaseDate;
+    map['title'] = title;
+    map['video'] = video;
+    map['vote_average'] = voteAverage;
+    map['vote_count'] = voteCount;
+    return map;
+  }
+
+}
--- lib/ui/movies_pages/search/search_screen.dart
+++ lib/ui/movies_pages/search/search_screen.dart
@@ -1,17 +1,125 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import '../../utils/color_resource/color_resources.dart';
+import 'api/SearchResponse.dart';
+import 'movie_list_item.dart';
+import 'api/search_api_constants.dart';
+import 'package:http/http.dart' as http;
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
 
   @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  final searchTextController = TextEditingController();
+  List<SearchResults>? searchResults;
+
+  @override
+  void initState() {
+    super.initState();
+    searchTextController.addListener(onSearchTextChanged);
+  }
+
+  @override
+  void dispose() {
+    searchTextController.dispose();
+    super.dispose();
+  }
+
+  void onSearchTextChanged() async {
+    final query = searchTextController.text;
+    if (query.isEmpty) {
+      setState(() {
+        searchResults = null;
+      });
+      return;
+    }
+
+    final results = await searchMovies(query);
+    setState(() {
+      searchResults = results.results;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Center(
-          child: Icon(
-            Icons.search,
-            color: Colors.white,
-            size: 60,
-          )),
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Column(
+      children: [
+        TextField(
+          cursorColor: ColorResources.yellow,
+          controller: searchTextController,
+          decoration: InputDecoration(
+            hintText: 'Search',
+            hintStyle: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.normal,
+                color: Color.fromARGB(255, 146, 145, 145)),
+            suffixIcon: IconButton(
+                onPressed: () {
+                  setState(() {
+                    searchTextController.clear();
+                  });
+                },
+                icon: Icon(Icons.clear)),
+            prefixIcon: Icon(
+              Icons.search_sharp,
+              color: Colors.white,
+              size: 25,
+            ),
+            focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(50),
+                borderSide: BorderSide(
+                  color: ColorResources.yellow,
+                  width: 2,
+                )),
+            enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(50),
+                borderSide: BorderSide(width: 1, color: Colors.white)),
+          ),
+        ),
+        if (searchResults != null && searchResults!.isEmpty ||
+            searchResults == null)
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/images/no_movie.png'),
+                SizedBox(height: height*.015,),
+                Text('No movies found'),
+              ],
+            ),
+          ),
+        if (searchResults != null && searchResults!.isNotEmpty)
+          Expanded(
+            child: ListView.builder(
+              itemCount: searchResults!.length,
+              itemBuilder: (context, index) {
+                final movie = searchResults![index];
+                return MovieListItem(movie: movie);
+              },
+            ),
+          )
+      ],
     );
   }
 }
+Future<SearchResponse> searchMovies(String query) async {
+  final apiKey = SearchApiConstants.apiKey; // Replace with your actual API key
+  final url = Uri.parse(
+    'https://api.themoviedb.org/3/search/movie?api_key=$apiKey&query=$query',
+  );
+  final response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    final data = jsonDecode(response.body);
+    return SearchResponse.fromJson(data);
+  } else {
+    throw Exception('Failed to load movies');
+  }
+}
+
